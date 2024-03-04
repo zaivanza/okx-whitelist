df05c139d8c1597611ee371428746ddc10d9b1e3
funnyruler7 <funnyruler7@gmail.com>
2024-03-04 09:19:19 +0300

    remove unused imports

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from loguru import logger
 from utils import OKX
 
+
 if __name__ == '__main__':
     session = OKX()
     session.main()
-    logger.succes("It's done!")
+    logger.success("It's done!")
