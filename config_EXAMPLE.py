860f20f9cababb0bc989e00915d56de542e03fee
funnyruler7 <funnyruler7@gmail.com>
2024-03-04 09:31:37 +0300

    config to pep8

--- config_EXAMPLE.py
+++ config_EXAMPLE.py
@@ -1,7 +1,7 @@
 # ================== Settings ==================
 
-chain   = 'Starknet' # ERC20 | OKTC | Arbitrum One | zkSync Lite | zkSync Era | Optimism | Harmony | Starknet
-token   = 'ETH' # ETH | ONE | CORE. To add other coins, you need to add them below in the links dictionary
+chain   = 'Starknet'  # ERC20 | OKTC | Arbitrum One | zkSync Lite | zkSync Era | Optimism | Harmony | Starknet
+token   = 'ETH'  # ETH | ONE | CORE. To add other coins, you need to add them below in the links dictionary
 
 OKX_2FA = "your_okx_2fa"
 EMAIL_LOGIN = "your_email_login"
@@ -9,9 +9,9 @@ EMAIL_2FA = "your_email_2fa"
 IMAP_URL = "imap.gmail.com"
 
 links = {
-    'ETH'   : {'link' : 'https://www.okx.cab/ru/balance/withdrawal-address/eth/2',  'token' : 'ETH'}, # it's for all the coins in the chains : ERC20 | OKTC | Arbitrum one | zkSync Lite | zkSync Era | Optimism | Starknet
-    'ONE'   : {'link' : 'https://www.okx.cab/ru/balance/withdrawal-address/one/1926',  'token' : 'ONE'}, # it's for the ONE coin on the Harmony network
-    'CORE'  : {'link' : 'https://www.okx.cab/ru/balance/withdrawal-address/core/2806', 'token' : 'CORE'}, # it's for the CORE coin on the CORE network
+    'ETH': {'link': 'https://www.okx.cab/ru/balance/withdrawal-address/eth/2',  'token': 'ETH'},  # it's for all the coins in the chains : ERC20 | OKTC | Arbitrum one | zkSync Lite | zkSync Era | Optimism | Starknet
+    'ONE': {'link': 'https://www.okx.cab/ru/balance/withdrawal-address/one/1926',  'token': 'ONE'},  # it's for the ONE coin on the Harmony network
+    'CORE': {'link': 'https://www.okx.cab/ru/balance/withdrawal-address/core/2806', 'token': 'CORE'},  # it's for the CORE coin on the CORE network
 }
 
 
