b9299a3ccdb7da9021cd372cde2c7df64a8d7be3
funnyruler7 <funnyruler7@gmail.com>
2024-03-21 21:32:10 +0300

    add traceback output

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
+from traceback import format_exc
 from loguru import logger
 from utils import OKX
 
 
 if __name__ == '__main__':
-    session = OKX()
-    session.main()
-    logger.success("It's done!")
+    try:
+        session = OKX()
+        session.main()
+        logger.success("It's done!")
+    except Exception as e:
+        sys.exit(f"Unexpected error: {e}\n\n"
+                 f"Open the issue at https://github.com/zaivanza/okx-whitelist/issues with traceback {format_exc()}")
